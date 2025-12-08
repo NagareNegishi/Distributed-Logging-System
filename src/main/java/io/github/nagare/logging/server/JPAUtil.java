2cb6762657eeacb95003d1ffd50a76a29848e9db
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-09 00:24:32 +1300

    Create class to manages EntityManagerFactory (unofficial way) #6

---
+++ src/main/java/io/github/nagare/logging/server/JPAUtil.java
@@ -0,0 +1,21 @@
+package io.github.nagare.logging.server;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+/**
+ * Util class for easy access to EntityManagerFactory -> Entity
+ * This is common pattern for project with JPA, however it will not manage Lifecycle of EntityManagerFactory
+ * While I implement it for test and future use, for this project I will use official way "ServletContextListener"
+ */
+public class JPAUtil {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("logDB");
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return emf;
+    }
+
+    public static void shutdown() {
+        emf.close();
+    }
+}
