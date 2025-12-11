2cd6d3cb280fcb2f9013d0d4da653b64427d4c81
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-11 18:27:01 +1300

    get EntityManagerFactory from Servlet #6

--- src/main/java/io/github/nagare/logging/server/LogEventRepository.java
+++ src/main/java/io/github/nagare/logging/server/LogEventRepository.java
@@ -1,15 +1,32 @@
 package io.github.nagare.logging.server;
 
+import jakarta.persistence.EntityManagerFactory;
+
 /**
  * Class handle Basic CRUD operations for LogServlet
  */
 public class LogEventRepository {
+
+    private final EntityManagerFactory emf;
+
+    /**
+     * Public constructor, servlet need to pass EntityManagerFactory
+     * @param emf EntityManagerFactory from Servlet or class use it
+     */
+    public LogEventRepository(EntityManagerFactory emf){
+        this.emf = emf;
+    }
 
     /**
      * LogServlet has Get, Post, Delete so at least need those 3 method
      */
 
     // Get need filter database based on level and limit, limit is irrelevant on database side
+
+
+
+
+
     // Post need to save
     // Delete need to delete
 
