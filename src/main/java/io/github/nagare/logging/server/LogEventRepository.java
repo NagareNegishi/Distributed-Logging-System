abfe0535d5b9fb66ad04eb77db183244a27bcd39
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-11 18:21:55 +1300

    set up LogEventRepository #6

---
+++ src/main/java/io/github/nagare/logging/server/LogEventRepository.java
@@ -0,0 +1,19 @@
+package io.github.nagare.logging.server;
+
+/**
+ * Class handle Basic CRUD operations for LogServlet
+ */
+public class LogEventRepository {
+
+    /**
+     * LogServlet has Get, Post, Delete so at least need those 3 method
+     */
+
+    // Get need filter database based on level and limit, limit is irrelevant on database side
+    // Post need to save
+    // Delete need to delete
+
+
+
+
+}
