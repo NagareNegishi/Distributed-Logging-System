d121e9fd3e0d19366393de2743543caa7ac320d1
negishnaga <negishnaga@ecs.vuw.ac.nz>
2025-12-11 20:25:01 +1300

    Database Setup for unit test #6

---
+++ src/test/java/io/github/nagare/logging/server/TestDatabaseSetup.java
@@ -0,0 +1,36 @@
+package io.github.nagare.logging.server;
+
+import jakarta.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+
+/**
+ * Utility class providing Database Setup for unit test.
+ */
+public class TestDatabaseSetup {
+
+    private static EntityManagerFactory emf;
+
+    public static EntityManagerFactory createTestEMF() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory("test-unit");
+        }
+        return emf;
+    }
+
+    public static void clearDatabase(EntityManagerFactory emf) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.createQuery("DELETE FROM LogEvent").executeUpdate();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+}
